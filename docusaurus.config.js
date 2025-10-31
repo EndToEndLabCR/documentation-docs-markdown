472d84a87fca108acc0b50cdf8b56e2f4b917f3f
Alonso <alonsonh94@gmail.com>
2025-10-30 22:50:24 -0600

    Initialize Docusaurus project setup

---
+++ docusaurus.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Documentation",
+  tagline: "Awesome Docs Powered by Docusaurus",
+  url: "https://EndToEndLabCR.github.io",
+  baseUrl: "/",
+  //   baseUrl: "/documentation-docs-markdown/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "EndToEndLabCR",
+  projectName: "documentation-docs-markdown",
+};
